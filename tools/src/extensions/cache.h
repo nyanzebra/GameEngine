e69bb538080fe10538e60af0140c8746cab5e9b3
nyanzebra <robbaldwin95@gmail.com>
2015-08-22 16:21:48 -0500

    Reformats class to be adherent to good standards

--- tools/src/extensions/cache.h
+++ tools/src/extensions/cache.h
@@ -1,5 +1,4 @@
-#ifndef CACHE_H
-#define CACHE_H
+#pragma once
 
 #include <map>
 #include <string>
@@ -9,47 +8,24 @@
 
 namespace cppe {
     namespace memory {
-        struct memory_unit { 
-            enum { value = 0 };
-        };
+        template <size_t Count> struct byte : public std::integral_constant<size_t, Count> {};
+        template <size_t Count> struct kilobyte : public std::integral_constant<size_t, byte<1024> * Count> {};
+        template <size_t Count> struct megabyte : public std::integral_constant<size_t, kilobyte<1024> * Count> {};
+        template <size_t Count> struct gigabyte : public std::integral_constant<size_t, megabyte<1024> * Count> {};
+        template <size_t Count> struct terabyte : public std::integral_constant<size_t, terabyte<1024> * Count> {};  
 
-        template<unsigned int Size = 1>
-        struct byte : memory_unit {
-            enum { value = Size };
-        };
-
-        template<unsigned int Size = 1>
-        struct kilobyte : memory_unit {
-            enum { value = 1024 * byte<Size - 1>::value; };
-        };
-
-        template<unsigned int Size = 1>
-        struct megabyte :memory_unit {
-            enum { value = 1024 * kilobyte<Size - 1>::value; };
-        };
-
-        template<unsigned int Size = 1>
-        struct gigabyte :memory_unit {
-            enum { value = 1024 * megabyte<Size - 1>::value; };
-        };
-
-        template<unsigned int Size = 1>
-        struct terabyte :memory_unit {
-            enum { value = 1024 * gigabyte<Size - 1>::value; };
-        };
-
-        template <class Capacity = kilobyte<1>,
+        template <size_t Capacity = kilobyte<1>::value,
             typename Key = int,
             typename Value = int,
             template<class, class, class, class> class Container = std::map,
             typename Comparator = std::less<Value>,
             typename Allocator = std::allocator < Value >>
         class cache {
-            typedef Container<const Key, std::weak_ptr<Value>, Comparator, Allocator> WeakCache;  
+            typedef Container<const Key, std::weak_ptr<Value>, Comparator, Allocator> WeakCache;
             typedef Container<const Key, std::shared_ptr<Value>, Comparator, Allocator> StrongCache;
             typedef std::pair<Key, Value> CachePairInstance;
         public:
-            cache(const char* name) : _name(name) { _name += ".ftlasset"; };
+            cache(const char* name) : _name(std::string(name) + ".ftlasset") {};
             cache(const cache&) = default;
             ~cache() { store(); }
 
@@ -77,8 +53,67 @@ namespace cppe {
             StrongCache _cache_ref;
             unsigned int _current_size = 0;
         };
+
+        template <class Capacity = kilobyte<1>::value, typename Key = int, typename Value = int, template<class, class, class, class> class Container = std::map, typename Comparator = std::less<Value>, typename Allocator = std::allocator<Value>>
+        void cache<Capacity, Key, Value, Container, Comparator, Allocator>::add(const Key& key, const Value& value) {
+            if (_current_size < Capacity / sizeof(char)) {
+                _current_size += sizeof(Value);
+                if (_cache.find(key) == _cache.end()) {
+                    _cache[key] = value;
+                    _cache_ref[key] = value;
+                }
+            } else {
+                store();
+            }
+        }
+
+        template <class Capacity = kilobyte<1>::value, typename Key = int, typename Value = int, template<class, class, class, class> class Container = std::map, typename Comparator = std::less<Value>, typename Allocator = std::allocator<Value>>
+        void cache<Capacity, Key, Value, Container, Comparator, Allocator>::remove(const Key& key) {
+            _cache.erase(key);
+        }
+
+        template <class Capacity = kilobyte<1>::value, typename Key = int, typename Value = int, template<class, class, class, class> class Container = std::map, typename Comparator = std::less<Value>, typename Allocator = std::allocator<Value>>
+        const Value&& cache<Capacity, Key, Value, Container, Comparator, Allocator>::operator[](const Key& key) {
+            auto sharedptr = _cache[key].lock();
+            return std::move(*sharedptr);
+        }
+
+        template <class Capacity = kilobyte<1>::value, typename Key = int, typename Value = int, template<class, class, class, class> class Container = std::map, typename Comparator = std::less<Value>, typename Allocator = std::allocator<Value>>
+        void cache<Capacity, Key, Value, Container, Comparator, Allocator>::clear() {
+            _cache.clear();
+        }
+
+        template <class Capacity = kilobyte<1>::value, typename Key = int, typename Value = int, template<class, class, class, class> class Container = std::map, typename Comparator = std::less<Value>, typename Allocator = std::allocator<Value>>
+        void cache<Capacity, Key, Value, Container, Comparator, Allocator>::store() {
+            cppe::io::file<Value> disk(_name);
+
+            std::pair<Key, Value>* values = new std::pair<Key, Value>[_cache.size()];
+            int i = 0;
+            for (auto it = _cache.begin(); it != _cache.end(); ++it) {
+                if (!it->second.expired()) {
+                    values[i++] = std::pair<Key, Value>(it->first, *it->second.lock().get());
+                }
+            }
+            disk.fill(values, Capacity * sizeof(char));
+            disk.write(true);
+            clear();
+        }
+
+        template <class Capacity = kilobyte<1>::value, typename Key = int, typename Value = int, template<class, class, class, class> class Container = std::map, typename Comparator = std::less<Value>, typename Allocator = std::allocator<Value>>
+        void cache<Capacity, Key, Value, Container, Comparator, Allocator>::fill() {
+            cppe::io::file<CachePairInstance> disk(_name);
+            disk.read(FILE_ALL, true);
+            for (int i = 0; i < Capacity / sizeof(Value) && i < sizeof(disk.data_ptr()) / sizeof(CachePairInstance); ++i) {
+                try {
+                    if (disk.data_ptr()) {
+                        auto it = disk.data_ptr()[i];
+
+                        _cache.insert(std::pair<Key, std::weak_ptr<Value>>(it.first, std::make_shared<Value>(it.second)));
+                    }
+                } catch (std::exception e) {
+                    console::output_line(e.what());
+                }
+            }
+        }
     }
 }
-
-#include "cache.cpp"
-#endif
